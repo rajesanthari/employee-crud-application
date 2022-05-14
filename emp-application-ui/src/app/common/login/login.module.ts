5d524fb8ff77efbbda340513655c866d093506bb
antharir <antharir@gmail.com>
2022-05-14 14:18:54 +0530

    Simple login page

---
+++ emp-application-ui/src/app/common/login/login.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { LoginComponent } from "./login.component";
+import { LoginRoutingModule } from "./login.routing.module";
+
+@NgModule({
+    declarations:[
+        LoginComponent
+    ],
+    imports:[
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        LoginRoutingModule
+    ]
+    
+})
+export class LoginModule {
+
+}
---
+++ emp-application-ui/src/app/employee/employee.routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { EmployeeAddEditDisplayComponent } from "./employee-add-edit-display/employee-add-edit-display.component";
+import { EmployeeListComponent } from "./employee-list/employee-list.component";
+
+const routes:Routes = [
+    {path:'', component:EmployeeListComponent},
+      {path:'add', component:EmployeeAddEditDisplayComponent, data:{isAdd:true}},
+      {path:':id', component:EmployeeAddEditDisplayComponent, data:{isEdit:false}},
+      {path:':id/edit', component:EmployeeAddEditDisplayComponent, data:{isEdit:true}}
+]
+
+@NgModule({
+    imports:[RouterModule.forChild(routes)],
+    exports:[RouterModule]
+})
+export class EmployeeRoutingModule {}
