e916180e22ad402ce04a6d599fee2ac780e5e9e4
antharir <antharir@gmail.com>
2022-05-17 22:38:18 +0530

    Spring mock test cases

--- angular-crud/src/test/java/com/ranthari/app/EmployeeApplicationTests.java
+++ angular-crud/src/test/java/com/ranthari/app/EmployeeApplicationTests.java
@@ -1,13 +1,64 @@
 package com.ranthari.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.ranthari.app.model.Employee;
+import com.ranthari.app.repository.EmployeeRepository;
+import com.ranthari.app.service.EmployeeService;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class EmployeeApplicationTests {
 
+	@Autowired
+	EmployeeService employeeService;
+
+	@MockBean
+	EmployeeRepository employeeRepository;
+
 	@Test
-	void contextLoads() {
+	void verifyAllUsers() {
+		when(employeeRepository.findAll()).thenReturn(
+				Arrays.asList(new Employee(101L, "Rajesh", "Anthari", "rajesh@gmail.com", "9553630497", 1200),
+						new Employee(102L, "Shiva", "Kumar", "shiva@gmail.com", "9848304697", 1400),
+						new Employee(103L, "Pavindar", "B", "pavan@gmail.com", "9440094400", 1600)));
+
+		assertEquals(3, employeeService.getAllEmployees().size());
+	}
+
+	@Test
+	void verifyUser() {
+		Employee employee = new Employee(101L, "Rajesh", "Anthari", "rajesh@gmail.com", "9553630497", 1200);
+		when(employeeRepository.findById(101L)).thenReturn(Optional.of(employee));
+
+		assertEquals(employee, employeeService.getAllEmployeeById(101L));
+	}
+
+
+	@Test
+	void verifySaveUser() {
+		Employee employee = new Employee(101L, "Rajesh", "Anthari", "rajesh@gmail.com", "9553630497", 1200);
+		when(employeeRepository.save(employee)).thenReturn(employee);
+
+		assertEquals(employee, employeeService.saveEmployee(employee));
+	}
+
+	@Test
+	void verifyDeleteUser() {
+		Employee employee = new Employee(101L, "Rajesh", "Anthari", "rajesh@gmail.com", "9553630497", 1200);
+		when(employeeRepository.findById(101L)).thenReturn(Optional.of(employee));
+		assertEquals(employee, employeeService.deleteEmployee(101L));
 	}
 
 }
---
+++ angular-crud/src/test/java/com/ranthari/app/EmployeeControllerTests.java
@@ -0,0 +1,51 @@
+package com.ranthari.app;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ranthari.app.model.Employee;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class EmployeeControllerTests {
+
+	MockMvc mockMvc;
+
+	@Autowired
+	WebApplicationContext context;
+
+	ObjectMapper objMapper = new ObjectMapper();
+
+	@Before
+	private void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+		System.out.println(mockMvc);
+	}
+
+	@Test
+	void verifySaveEmmployee() throws Exception {
+		Employee employee = new Employee("Rajesh", "Anthari", "rajesh@gmail.com", "9553630497", 1200);
+		String empString = objMapper.writeValueAsString(employee);
+		MvcResult result = mockMvc
+				.perform(post("/employee").content(empString).contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isOk()).andReturn();
+		String resultContent = result.getResponse().getContentAsString();
+		Employee createdEmployee = objMapper.readValue(resultContent, Employee.class);
+		System.out.println(resultContent);
+		System.out.println(createdEmployee);
+	}
+
+}
