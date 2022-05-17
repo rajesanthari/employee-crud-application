01aa10c7a93e93db7582071524bf941795a86c20
antharir <antharir@gmail.com>
2022-05-17 10:39:53 +0530

    added ref link for CORS

--- angular-crud/src/main/java/com/ranthari/app/AppConfiguration.java
+++ angular-crud/src/main/java/com/ranthari/app/AppConfiguration.java
@@ -9,6 +9,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class AppConfiguration {
 
+	
+	/**
+	 * Ref url: https://stackoverflow.com/questions/36968963/how-to-configure-cors-in-a-spring-boot-spring-security-application/37610988#37610988
+	 * @return
+	 */
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
