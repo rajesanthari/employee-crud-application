fbd7f88b361f4317a6998cb442bf6c9c1db3519d
antharir <antharir@gmail.com>
2022-05-19 17:12:09 +0530

    leet code

--- angular-crud/src/test/java/com/ranthari/app/LeetCode.java
+++ angular-crud/src/test/java/com/ranthari/app/LeetCode.java
@@ -1,7 +1,15 @@
 package com.ranthari.app;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Arrays;
 
+/**
+ * @author ranthari
+ *
+ */
 public class LeetCode {
 
 	public static void removeElementMain(String[] args) {
@@ -69,7 +77,7 @@ public class LeetCode {
 
 	}
 
-	public static void main(String[] args) {
+	public static void canPlaceFlowersMain(String[] args) {
 		int[] arr = { 1, 0, 0, 0, 1 };
 		int n = 3;
 		boolean result = canPlaceFlowers(arr, n);
@@ -81,7 +89,7 @@ public class LeetCode {
 		int count = 0;
 		for (int k = 0; k < arr.length; k++) {
 			if (arr[k] == 0) {
-				int prev = (k == 0 || arr[k-1] == 0) ? 0 : 1;
+				int prev = (k == 0 || arr[k - 1] == 0) ? 0 : 1;
 				int next = (k == arr.length - 1 || arr[k + 1] == 0) ? 0 : 1;
 				if (prev == 0 && next == 0) {
 					count++;
@@ -95,4 +103,84 @@ public class LeetCode {
 
 		return false;
 	}
+
+	/**
+	 * @param args
+	 * @throws ClassNotFoundException
+	 */
+	public static void sqlServerConnectionTest(String[] args) throws ClassNotFoundException {
+
+		Connection conn = null;
+
+		try {
+
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			String dbURL = "jdbc:sqlserver://SQLSERVER;databaseName=SQLDB";
+			String user = "user";
+			String pass = "password";
+			conn = DriverManager.getConnection(dbURL, user, pass);
+			if (conn != null) {
+				DatabaseMetaData dm = (DatabaseMetaData) conn.getMetaData();
+				System.out.println("Driver name: " + dm.getDriverName());
+				System.out.println("Driver version: " + dm.getDriverVersion());
+				System.out.println("Product name: " + dm.getDatabaseProductName());
+				System.out.println("Product version: " + dm.getDatabaseProductVersion());
+			}
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * https://leetcode.com/problems/plus-one/
+	 * 
+	 * @param args
+	 */
+	public static void plusOneMain(String[] args) {
+		int[] arr = { 1, 2, 3 };
+		int[] mod = plusOne(arr);
+		System.out.println(Arrays.toString(mod));
+	}
+
+	public static int[] plusOne(int[] digits) {
+
+		int car = 1;
+
+		for (int k = digits.length - 1; k >= 0; k--) {
+			int sum = digits[k] + car;
+			car = 0;
+			if (sum > 9) {
+				car = 1;
+				sum = sum % 10;
+			}
+			digits[k] = sum;
+		}
+
+		return addCar(digits, car);
+	}
+
+	public static int[] addCar(int[] digits, int car) {
+
+		if (car == 1) {
+
+			int[] newArr = new int[digits.length + 1];
+			newArr[0] = car;
+			int counter = 1;
+			for (int num : digits) {
+				newArr[counter] = num;
+			}
+			digits = newArr;
+		}
+		return digits;
+	}
+
 }
